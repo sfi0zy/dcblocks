b039fdcad64e5212e484a1e33464508f35d34737
sfi0zy <sfi0zy@gmail.com>
2015-03-08 16:15:43 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+#DCBlocks
+
+This is extension for Brackets, which adds ability to easy creation of comment blocks for DOM elements in HTML/XML code.
+
+###Installing
+Clone this repository and move it in Brackets extensions folder (*Help > Show Extensions Folder*), than reload Brackets.
+
+###Usage
+Usage is "Emmet-like": type any word and press "Ctrl+B". For example You write
+```html
+header
+```
+And now press this keys, You'll see this:
+```html
+<!-- HEADER --><!-- END HEADER -->
+```
+
+###License
+MIT
---
+++ main.js
@@ -0,0 +1,49 @@
+/*
+ * DCBlocks - Generator of comment blocks for DOM elements in HTML/XML code for Brackets
+ * Copyright (c) 2015 Ivan Bogachev <sfi0zy@gmail.com> (http://sfi0zy.github.io/)
+ * Free to use under MIT license
+ */
+
+define(function (require, exports, module) {
+    "use strict";
+
+    var CommandManager    = brackets.getModule("command/CommandManager"),
+        EditorManager     = brackets.getModule("editor/EditorManager"),
+        KeyBindingManager = brackets.getModule("command/KeyBindingManager");
+    
+    var getCommentContent = function (line, cursorPosition) {
+        var words;
+        line = line.substring(0, cursorPosition);
+        words = line.split(/\W/);
+        return words[words.length - 1];
+    };
+
+    function createCommentForBlock() {
+        var editor, content, cursorPosition, newCursorPosition;
+        
+        editor = EditorManager.getFocusedEditor();
+        
+        if (editor) {
+            cursorPosition = editor.getCursorPos();
+            content = getCommentContent(editor.document.getLine(cursorPosition.line), cursorPosition.ch).toUpperCase();
+
+            editor.document.replaceRange("", {
+                line: cursorPosition.line,
+                ch:   cursorPosition.ch - content.toString().length
+            }, cursorPosition);
+            
+            cursorPosition = editor.getCursorPos();
+            editor.document.replaceRange("<!-- " + content + " -->", cursorPosition);
+            newCursorPosition = editor.getCursorPos();
+            editor.document.replaceRange("<!-- END " + content + " -->", newCursorPosition);
+            editor.setCursorPos(newCursorPosition.line, newCursorPosition.ch);
+        }
+    }
+
+    var CID_CREATE_COMMENT = "dcblocks-brackets.createCommentForBlock";
+    
+    CommandManager.register("Create comment for block", CID_CREATE_COMMENT, createCommentForBlock);
+    
+    KeyBindingManager.removeBinding("Ctrl-B"); // "Ctrl-B" combination is already assigned to cmd.addNextMatch
+    KeyBindingManager.addBinding(CID_CREATE_COMMENT, "Ctrl-B");
+});
